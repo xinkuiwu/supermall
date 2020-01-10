ae268f638b2538080c1cfd41b1b258943614b35e
xinkuiwu <1079894635@qq.com>
2020-01-10 15:49:53 +0800

    cartDone

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
 import toast from 'components/common/toast'
 
 Vue.config.productionTip = false
@@ -10,6 +13,12 @@ Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 
 Vue.use(toast)
+
+FastClick.attach(document.body)
+
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/img/common/placeholder.png')
+})
 
 new Vue({
   render: h => h(App),
